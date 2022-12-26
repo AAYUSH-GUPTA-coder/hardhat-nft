c85b6bff1f936d264664e511a2acca107450bf5e
Aayush Gupta <guptaaayush432@gmail.com>
2022-12-26 15:56:19 +0530

    initial commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,73 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL
+
 module.exports = {
-  solidity: "0.8.17",
-};
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            forking: {
+                url: MAINNET_RPC_URL,
+            },
+            blockConfirmation: 1,
+        },
+        goerli: {
+            chainId: 5,
+            blockConfirmation: 6,
+            url: GOERLI_RPC_URL,
+            accounts: [GOERLI_PRIVATE_KEY],
+        },
+    },
+    etherscan: {
+        apiKey: {
+            goerli: ETHERSCAN_API_KEY,
+        },
+    },
+    gasReporter: {
+        enabled: true,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        coinmarketcap: COINMARKETCAP_API_KEY,
+    },
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.7",
+            },
+            {
+                version: "0.6.6",
+            },
+            {
+                version: "0.6.12",
+            },
+            {
+                version: "0.4.19",
+            },
+        ],
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
+    mocha: {
+        timeout: {
+            timeout: 2000000, // 200 seconds
+        },
+    },
+}
